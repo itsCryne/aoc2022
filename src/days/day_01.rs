8af041c153c48b49a8063422259e0fae513cedc4
Patrick <secure@cryne.me>
2022-12-01 06:12:00 +0100

    Day 01

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -1,9 +1,33 @@
 pub(crate) fn a(input: &str) -> usize {
-    let lines = input.lines();
-    return 0;
+    let lines: Vec<&str> = input.lines().collect();
+    let mut cals = vec![0;lines.len()];
+    let mut elf = 0;
+
+    for line in lines {
+        if line == "" {
+            elf += 1;
+        } else {
+            cals[elf] += usize::from_str_radix(line, 10).unwrap();
+        }
+    }
+
+    return *cals.iter().max().unwrap();
 }
 pub(crate) fn b(input: &str) -> usize {
-    let lines = input.lines();
-    return 0;
+    let lines: Vec<&str> = input.lines().collect();
+    let mut cals = vec![0;lines.len()];
+    let mut elf = 0;
+
+    for line in lines {
+        if line == "" {
+            elf += 1;
+        } else {
+            cals[elf] += usize::from_str_radix(line, 10).unwrap();
+        }
+    }
+
+    cals.sort_unstable_by(|a, b| b.cmp(a));
+
+    return cals[0] + cals[1] + cals[2];
 }
 
