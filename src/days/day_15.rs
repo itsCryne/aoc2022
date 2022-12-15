52a3c85f00830c8ea433ce3d22d3ed7c6d9e53e1
Patrick <secure@cryne.me>
2022-12-15 15:50:33 +0100

    Day 15: Use Rayon

--- src/days/day_15.rs
+++ src/days/day_15.rs
@@ -1,5 +1,7 @@
 use std::{fmt::Display, collections::{HashMap, HashSet}};
 
+use rayon::prelude::{IntoParallelIterator, ParallelIterator, IntoParallelRefIterator};
+
 fn manhattan_distance(l1: (i64, i64), l2: (i64, i64)) -> i64 {
     (l1.0.abs_diff(l2.0) + l1.1.abs_diff(l2.1)) as i64
 }
@@ -24,30 +26,38 @@ pub fn a(input: &str) -> Box<dyn Display> {
         locations.insert((sx, sy), manhattan_distance((sx, sy), (bx, by)));
     }
 
-    let min_x = locations.iter().fold(i64::MAX, |acc, cur| {
+    let min_x = locations.par_iter().fold(|| i64::MAX, |acc, cur| {
         if cur.0.0 - (*cur.1 as i64) < acc {
             cur.0.0 - (*cur.1 as i64)
         } else {
             acc
         }
-    });
+    }).min().unwrap();
 
-    let max_x = locations.iter().fold(i64::MIN, |acc, cur| {
+    let max_x = locations.par_iter().fold(|| i64::MIN, |acc, cur| {
         if cur.0.0 + (*cur.1 as i64) > acc {
             cur.0.0 + (*cur.1 as i64)
         } else {
             acc
         }
-    });
-
+    }).max().unwrap();
 
     let y = 2000000;
-    let mut counter = 0;
+    let counter = (min_x..max_x).into_par_iter().filter_map(|x| {
+        if close_to_sensor((x, y), &locations) && !beacons.contains(&(x,y)) {
+            Some(1)
+        } else {
+            None
+        }
+    }).count();
+
+    /*
     for x in min_x..=max_x {
         if close_to_sensor((x, y), &locations) && !beacons.contains(&(x,y)) {
             counter += 1;
         }
     }
+    */
 
     Box::new(counter)
 }
@@ -67,30 +77,21 @@ pub fn b(input: &str) -> Box<dyn Display> {
         beacons.insert((bx, by));
         locations.insert((sx, sy), manhattan_distance((sx, sy), (bx, by)));
     }
-
-    let search_space_size = 4000000;
-    for (s, d) in &locations {
-        let mut outside = HashSet::new();
-
-        outside.insert((s.0-(d+1), s.1));
-        outside.insert((s.0+(d+1), s.1));
-
-        for x in (s.0-*d)..=(s.0+*d) {
+    
+    let pos = locations.par_iter().find_map_any(|(s, d)| {
+        ((s.0-*d)..=(s.0+*d)).into_par_iter().flat_map(|x| {
             let p1 = (x, (s.1 - (*d - (x.abs_diff(s.0) as i64)))-1);
             let p2 = (x, s.1 + (*d - (x.abs_diff(s.0) as i64))+1);
 
-            outside.insert(p1);
-            outside.insert(p2);
-        }
+            [p1, p2]
+        }).find_map_any(|(x,y)| {
+            if !close_to_sensor((x, y), &locations) && !beacons.contains(&(x, y)) {
+                Some((x, y))
+            } else {
+                None
+            }
+        })
+    }).unwrap();
 
-        for (x,y) in outside {
-            if !(0..=search_space_size).contains(&x) || !(0..=search_space_size).contains(&y) {
-                continue;
-            }
-            if !close_to_sensor((x, y), &locations) && !beacons.contains(&(x,y)) {
-                return Box::new(x * 4000000 + y);
-            }
-        }
-    }
-    unreachable!()
+    Box::new(pos.0 * 4000000 + pos.1)
 }
